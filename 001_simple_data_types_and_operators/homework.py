540426f54beab731fdd312a11bbf8f857b4bc2cc
Roman Kutslepa <roman.kutselepa@gmail.com>
2023-12-18 23:53:39 +0200

    simple data types

---
+++ 001_simple_data_types_and_operators/homework.py
@@ -0,0 +1,27 @@
+# Write a program that:
+# 1. Calculates age from the given data (current_year - year of birth)
+# 2. Find the missing part of the code (code_2)
+#     a. Find the remainder of x divided by y
+#     b. Multiply the obtained result by 13
+#     c. Take the square root of the result
+#     d. Take the integer part of the result
+# 3. Combine the code into a separate variable
+#     Example:
+#         475-12-967
+# 4. Print the string:
+#     Example:
+#         Hello Mary Gold. You are 26 years old. Your secret code is 475-12-967.
+
+
+# years
+current_year = 2023
+year_of_birth = 1988
+# code parts
+code_1 = '354'
+code_3 = 132
+# name
+user_name = 'John'
+user_surname = 'Smith'
+# code 2 data
+x = 152
+y = 132
