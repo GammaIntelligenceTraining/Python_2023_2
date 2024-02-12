2b5314e00c8a5b1702d496a51c03c0a0a8a7c8b1
Roman Kutslepa <roman.kutselepa@gmail.com>
2024-02-12 16:44:27 +0200

    Requests, bs4

--- 016_requests/currency_converter.py
+++ 016_requests/currency_converter.py
@@ -1,39 +1,22 @@
-from bs4 import BeautifulSoup as BS
 import requests
+from bs4 import BeautifulSoup
 
-def user_choice():
-    try:
-        user_input = input('Please choose:\n1. Convert RUB to EUR\n2. Convert EUR to RUB\n3. Exit\n-->')
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
+                         ' (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36'}
+usd_yen_url = 'https://www.google.com/search?q=usd+to+yen&sca_esv=9a94bee5ed3f9006&sxsrf=ACQVn0-3p2Zd7yjiULl7O1O5__1M3TMbIg%3A1707747351641&source=hp&ei=FyjKZY2DJcLUxc8Pw4a2gAg&iflsig=ANes7DEAAAAAZco2J_lnlI_Kf300aFQ3sOGb8E27TaKz&ved=0ahUKEwjN6ojS_qWEAxVCavEDHUODDYAQ4dUDCA0&uact=5&oq=usd+to+yen&gs_lp=Egdnd3Mtd2l6Igp1c2QgdG8geWVuMg8QIxiABBiKBRgnGEYYggIyChAAGIAEGBQYhwIyBRAAGIAEMgUQABiABDIFEAAYgAQyBRAAGIAEMgUQABiABDIFEAAYgAQyBRAAGIAEMgUQABiABEjwVlC2QViBUXABeACQAQCYAVWgAcIEqgECMTC4AQPIAQD4AQGoAgrCAgcQIxjqAhgnwgIKECMYgAQYigUYJ8ICCxAAGIAEGIoFGJECwgIQEC4YgAQYigUYQxjHARjRA8ICCxAuGIAEGMcBGNEDwgIFEC4YgATCAgoQABiABBiKBRhDwgIHEAAYgAQYCg&sclient=gws-wiz'
+eur_aud_url = 'https://www.google.com/search?q=eur+to+aud&sca_esv=9a94bee5ed3f9006&sxsrf=ACQVn08XlpfbiarJMnReEHtokpBYfXsTNw%3A1707747364743&ei=JCjKZfv_LLu-wPAP8PyS8Ac&ved=0ahUKEwj7oarY_qWEAxU7HxAIHXC-BH4Q4dUDCBA&uact=5&oq=eur+to+aud&gs_lp=Egxnd3Mtd2l6LXNlcnAiCmV1ciB0byBhdWQyChAAGIAEGIoFGEMyBRAAGIAEMgUQABiABDIFEAAYgAQyBRAAGIAEMgUQABiABDIFEAAYgAQyBRAAGIAEMgUQABiABDIFEAAYgARIiEFQvxZYozxwBngBkAEAmAGzAaABtgmqAQQxNy4xuAEDyAEA-AEBqAIUwgIKEAAYRxjWBBiwA8ICDRAAGIAEGIoFGEMYsAPCAgsQABiABBiKBRiRAsICChAjGIAEGIoFGCfCAg8QIxiABBiKBRgnGEYYggLCAhkQABiABBiKBRhGGIICGJcFGIwFGN0E2AEBwgIHECMY6gIYJ8ICExAAGIAEGIoFGEMY6gIYtALYAQLCAhAQLhjHARjRAxiABBiKBRgnwgILEC4YgAQYxwEY0QPCAgwQABiABBiKBRhDGArCAh0QLhjHARjRAxiABBiKBRiXBRjcBBjeBBjgBNgBA8ICDxAAGIAEGIoFGEMYRhiCAsICGxAAGIAEGIoFGEMYRhiCAhiXBRiMBRjdBNgBAcICChAAGIAEGBQYhwLiAwQYACBBiAYBkAYKugYGCAEQARgTugYGCAIQARgBugYGCAMQARgU&sclient=gws-wiz-serp'
 
-        if user_input == '1':
-            amount = float(input('Please enter amount:\n-->'))
-            result = amount * get_currency_value()
-            result_string = '{result:.2f} Euros'
-            print(result_string.format(result=result))
-            user_choice()
-        elif user_input == '2':
-            amount = float(input('Please enter amount:\n-->'))
-            result = amount / get_currency_value()
-            result_string = '{result:.2f} Rubles'
-            print(result_string.format(result=result))
-            user_choice()
-        elif user_input == '3':
-            print('Good Bye')
-        else:
-            print('Choice is out of range')
-            user_choice()
-    except:
-        print('Error')
-        user_choice()
+def convert(url):
+    response = requests.get(url, headers=headers)
+    if response.ok:
+        soup = BeautifulSoup(response.content, 'html.parser')
+        element = soup.find('span', class_='DFlfde SwHCTb')
+        return float(element.get('data-value'))
+        # rate = float(element.string.replace(',', '.'))
+        # return rate
+    else:
+        print('Error connecting.')
 
-def get_currency_value():
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36'}
-    url = 'https://www.google.com/search?source=hp&ei=VQNJYLbWN_GSrgSEzbbwBQ&iflsig=AINFCbYAAAAAYEkRZUlSbXa0t2sgKC5ci_aZbu3dSgpn&q=rub+to+eur&oq=rub+to+eur&gs_lcp=Cgdnd3Mtd2l6EAMyBwgAEEYQggIyAggAMgIIADICCAAyAggAMgIIADICCAAyAggAMgIIADICCAA6BQgAELEDOgUILhCxAzoLCAAQsQMQxwEQowI6CAguELEDEIMBOggIABCxAxCDAToLCAAQsQMQxwEQrwE6AgguUPyVGVi-txlgyMUZaAFwAHgAgAFfiAGqBJIBAjExmAEAoAEBqgEHZ3dzLXdpeg&sclient=gws-wiz&ved=0ahUKEwj21Ja5oabvAhVxiYsKHYSmDV4Q4dUDCAY&uact=5'
-    full_page = requests.get(url, timeout=5, headers=headers)
-    soup = BS(full_page.content, 'html.parser')
-    currency_value = soup.findAll('span', class_="DFlfde SwHCTb")
-    print(currency_value[0]['data-value'])
-    result = currency_value[0].text.replace(',', '.')
-    return float(result)
-
-user_choice()
+print(100 * convert(usd_yen_url))
+# convert(usd_yen_url)
+print(100 * convert(eur_aud_url))
--- 016_requests/tester.py
+++ 016_requests/tester.py
@@ -1,9 +1,51 @@
 import requests
+from bs4 import BeautifulSoup
+from requests.exceptions import HTTPError
 
 # 200 - 299 Success
 # 300 - 399 Redirect
 # 400 - 499 Client error
 # 500 - 599 Server error
+# headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
+#                          ' (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36'}
+#
+# r = requests.get('https://xkcd.com/353/')
+#
+# soup = BeautifulSoup(r.content, 'html.parser')
+# print(soup.a['href'])
+# match = soup.find('div', id='middleContainer')
+# print(match.div.text)
 
-r = requests.get('https://xkcd.com/353/', timeout=10)
-print(r.ok)
+# matches = soup.find_all('div', class_='box')
+# print(len(matches))
+# print(matches)
+# for match in matches:
+#     print(match.get_attribute_list('class'))
+# md_container = soup.find('div', id='middleContainer')
+# matches = md_container.find_all('a')
+# for match in matches:
+#     if 'https://' not in match['href']:
+#         print('https://xkcd.com', match['href'], sep='')
+#     else:
+#         print(match['href'])
+
+# match = soup.find_all('div', string='Python')
+import re
+# match = soup.find_all('a', href=True, limit=2)
+# for match in match:
+#     print(match)
+
+# first_link = soup.find('a', href='https://xkcd.com/353')
+# print(first_link.chi)
+# first_div = soup.find('div')
+# print(list(first_div.strings))
+
+
+import datetime
+url = 'https://dashboard.elering.ee/api/nps/price?start=2024-02-12T20%3A59%3A59.999Z&end=2024-02-14T20%3A59%3A59.999Z'
+response = requests.get(url)
+json_data = response.json()
+for day in json_data['data']['ee']:
+    dt = datetime.datetime.fromtimestamp(day['timestamp'])
+    price = day['price']
+    print(dt, price)
