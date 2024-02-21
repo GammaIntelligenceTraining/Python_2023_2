611f3d77d4b447f1eb92f0a275292818cdd8d0c6
Roman Kutselepa <roman.kutselepa@gmail.com>
2024-02-21 16:53:16 +0200

    homework

---
+++ 021_os_module/homework.py
@@ -0,0 +1,4 @@
+# for files in "unsorted" folder
+# write a program, that checks file extensions
+# and sorts them to different folders corresponding to file type
+# Documents (*.txt, *.html), music (*.mp3), images (*.png, *.jpg)
